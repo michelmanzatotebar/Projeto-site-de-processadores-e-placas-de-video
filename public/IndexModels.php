3accae923a929ac3e670bdbd04ddfcad51788a47
michelmanzatotebar <zsouta22@gmail.com>
2024-11-11 20:14:48 -0400

    Atualizando api

--- public/IndexModels.php
+++ public/IndexModels.php
@@ -1,43 +1,51 @@
 <?php
-require_once '../config/db.php';
-require_once '../controllers/usuarioController.php';
-require_once '../Router.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . '/../src/config/db.php';
+require_once __DIR__ . '/../src/routes/Router.php';
+require_once __DIR__ . '/../src/controllers/CarrinhoController.php';
+require_once __DIR__ . '/../src/controllers/ClienteController.php';
+require_once __DIR__ . '/../src/controllers/PedidoController.php';
+require_once __DIR__ . '/../src/controllers/ProdutoController.php';
 
 $router = new Router();
 $carrinhoController = new CarrinhoController($pdo);
+$clienteController = new ClienteController($pdo);
 $pedidoController = new PedidoController($pdo);
 $produtoController = new ProdutoController($pdo);
-$clienteController = new ClienteController($pdo);
 
-header("Content-type: application/json; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
 
-$router->add('GET','/Pedido', [$pedidoController, 'list']);
-$router->add('GET','/Pedido/{id}', [$pedidoController, 'getById']);
-$router->add('REMOVE','/Pedido/{id}', [$pedidoController, 'delete']);
-$router->add('POST','/Pedido', [$pedidoController, 'getById']);
-$router->add('PUT','/Pedido', [$pedidoController, 'getById']);
+$router->add('GET', '/Carrinho', [$carrinhoController, 'list']);
+$router->add('GET', '/Carrinho/{id}', [$carrinhoController, 'getById']);
+$router->add('POST', '/Carrinho', [$carrinhoController, 'create']);
+$router->add('PUT', '/Carrinho/{id}', [$carrinhoController, 'update']);
+$router->add('DELETE', '/Carrinho/{id}', [$carrinhoController, 'delete']);
 
-$router->add('GET','/Produto', [$produtoController, 'list']);
-$router->add('GET','/Produto/{id}', [$produtoController, 'getById']);
-$router->add('REMOVE','/Produto/{id}', [$produtoController, 'delete']);
-$router->add('POST','/Produto', [$produtoController, 'getById']);
-$router->add('PUT','/Produto', [$produtoController, 'getById']);
+$router->add('GET', '/Cliente', [$clienteController, 'list']);
+$router->add('GET', '/Cliente/{id}', [$clienteController, 'getById']);
+$router->add('POST', '/Cliente', [$clienteController, 'create']);
+$router->add('PUT', '/Cliente/{id}', [$clienteController, 'update']);
+$router->add('DELETE', '/Cliente/{id}', [$clienteController, 'delete']);
 
-$router->add('GET','/Carrinho', [$carrinhoController, 'list']);
-$router->add('GET','/Carrinho/{id}', [$carrinhoController, 'getById']);
-$router->add('REMOVE','/Carrinho/{id}', [$carrinhoController, 'delete']);
-$router->add('POST','/Carrinho', [$carrinhoController, 'getById']);
-$router->add('PUT','/Carrinho', [$carrinhoController, 'getById']);
+$router->add('GET', '/Pedido', [$pedidoController, 'list']);
+$router->add('GET', '/Pedido/{id}', [$pedidoController, 'getById']);
+$router->add('POST', '/Pedido', [$pedidoController, 'create']);
+$router->add('PUT', '/Pedido/{id}', [$pedidoController, 'update']);
+$router->add('DELETE', '/Pedido/{id}', [$pedidoController, 'delete']);
 
-
-$router->add('GET','/Cliente', [$clienteController, 'list']);
-$router->add('GET','/Cliente/{id}', [$clienteController, 'getById']);
-$router->add('REMOVE','/Cliente/{id}', [$clienteController, 'delete']);
-$router->add('POST','/Cliente', [$clienteController, 'getById']);
-$router->add('PUT','/Cliente', [$clienteController, 'getById']);
+$router->add('GET', '/Produto', [$produtoController, 'list']);
+$router->add('GET', '/Produto/{id}', [$produtoController, 'getById']);
+$router->add('POST', '/Produto', [$produtoController, 'create']);
+$router->add('PUT', '/Produto/{id}', [$produtoController, 'update']);
+$router->add('DELETE', '/Produto/{id}', [$produtoController, 'delete']);
 
 $requestedPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-$pathItems = explode("/", $requestedPath);
-$requestedPath = "/" . $pathItems[3] . ($pathItems[4] ? "/" . $pathItems[4] : "");
+$basePath = '/Projeto-site-de-processadores-e-placas-de-video/public'; 
+$requestedPath = str_replace($basePath, '', $requestedPath);
 
 $router->dispatch($requestedPath);
--- src/routes/Router.php
+++ src/routes/Router.php
@@ -6,20 +6,37 @@ class Router
 
     public function add($method, $path, $callback)
     {
-        $path = preg_replace('/{(\w+)}/', '(\d+)', $path);
-        $this->routes[] = ['method' => $method, 'path' => "#^" . $path . "$#", 'callback' => $callback];
+        $path = preg_replace('/{(\w+)}/', '([^/]+)', $path);
+        $this->routes[] = [
+            'method' => $method,
+            'path' => "#^" . $path . "$#",
+            'callback' => $callback
+        ];
     }
 
     public function dispatch($requestedPath)
     {
         $requestedMethod = $_SERVER["REQUEST_METHOD"];
-
+        error_log("Requested Path: " . $requestedPath);
+        error_log("Request Method: " . $requestedMethod);
+        
         foreach ($this->routes as $route) {
             if ($route['method'] === $requestedMethod && preg_match($route['path'], $requestedPath, $matches)) {
                 array_shift($matches);
-                return call_user_func($route['callback'], $matches[0]);
+                try {
+                    $result = call_user_func($route['callback'], ...$matches);
+                    header('Content-Type: application/json');
+                    echo json_encode($result);
+                    return;
+                } catch (Exception $e) {
+                    http_response_code(500);
+                    echo json_encode(['error' => $e->getMessage()]);
+                    return;
+                }
             }
         }
-        echo "404 - Página não encontrada";
+        
+        http_response_code(404);
+        echo json_encode(['error' => '404 - Rota não encontrada']);
     }
 }
