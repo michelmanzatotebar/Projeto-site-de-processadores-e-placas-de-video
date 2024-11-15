08256fd574d73a1c99f6b81d5fc5296b0e9f8755
michelmanzatotebar <zsouta22@gmail.com>
2024-11-14 20:57:30 -0400

    Arrumando rotas e index

--- src/routes/Router.php
+++ src/routes/Router.php
@@ -1,42 +1,29 @@
 <?php
-
+require_once __DIR__ . '/../models/Carrinho.php';
 class Router
 {
     private $routes = [];
 
     public function add($method, $path, $callback)
     {
-        $path = preg_replace('/{(\w+)}/', '([^/]+)', $path);
-        $this->routes[] = [
-            'method' => $method,
-            'path' => "#^" . $path . "$#",
-            'callback' => $callback
-        ];
+        $path = preg_replace('/\{(\w+)\}/', '(\d+)', $path);
+        $this->routes[] = ['method' => $method, 'path' => "#^" . $path . "$#", 'callback' => $callback];
     }
-
+ 
     public function dispatch($requestedPath)
     {
         $requestedMethod = $_SERVER["REQUEST_METHOD"];
-        error_log("Requested Path: " . $requestedPath);
-        error_log("Request Method: " . $requestedMethod);
-        
+        echo "     - METODO NO ROUTER: " . $requestedMethod; 
+        echo "     - PATH REQUISITADO NO ROUTER: " . $requestedPath; 
+
         foreach ($this->routes as $route) {
             if ($route['method'] === $requestedMethod && preg_match($route['path'], $requestedPath, $matches)) {
                 array_shift($matches);
-                try {
-                    $result = call_user_func($route['callback'], ...$matches);
-                    header('Content-Type: application/json');
-                    echo json_encode($result);
-                    return;
-                } catch (Exception $e) {
-                    http_response_code(500);
-                    echo json_encode(['error' => $e->getMessage()]);
-                    return;
-                }
+                echo(" - Deucerto PAth - ");
+                return call_user_func($route['callback'], $matches[0]);
             }
         }
-        
-        http_response_code(404);
-        echo json_encode(['error' => '404 - Rota não encontrada']);
+        echo " (404 - Página não encontrada)";
     }
 }
+
